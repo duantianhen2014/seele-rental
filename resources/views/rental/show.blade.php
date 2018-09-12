8e885b79c8694b965801e711117863aca257ba86
XiaoTeng <13675626825@163.com>
2018-09-12 14:56:58 +0800

    rental show page adjust

--- resources/views/rental/show.blade.php
+++ resources/views/rental/show.blade.php
@@ -10,64 +10,83 @@
                         <p class="text-center">Rental Information</p>
                     </div>
                     <div class="panel-body">
-                        <p>A Address: {{$rental->a_address}}</p>
-                        <p>B Address: {{$rental->b_address}}</p>
-                        <p>Charge: {{$rental->charge}} seele</p>
-                        <p>Deposit Money: {{$rental->deposit}} seele</p>
-                        <p>Status: {{$rental->statusText()}}</p>
-                        @if($rental->a_apply_tx_hash)
-                            <p>A Apply Hash:{{$rental->a_apply_tx_hash}}</p>
-                        @endif
-                        @if($rental->b_confirm_tx_hash)
-                            <p>B Confirm Hash:{{$rental->b_confirm_tx_hash}}</p>
-                        @endif
-                        @if($rental->a_confirm_tx_hash)
-                            <p>A Confirm Hash:{{$rental->a_confirm_tx_hash}}</p>
-                        @endif
-                        @if($rental->a_complete_apply_tx_hash)
-                            <p>A Complete Hash:{{$rental->a_complete_apply_tx_hash}}</p>
-                        @endif
-                        @if($rental->b_complete_tx_hash)
-                            <p>B Complete Hash:{{$rental->b_complete_tx_hash}}</p>
-                        @endif
+                        <table class="table table-hover table-bordered">
+                            <tr>
+                                <td>A Address: {{$rental->a_address}}</td>
+                                <td>B Address: {{$rental->b_address}}</td>
+                                <td>
+                                    @if($rental->product->user_id == Auth::id())
 
-                        @if($rental->product->user_id == Auth::id())
+                                        @if($rental->status == \App\Models\Rental::STATUS_A_APPLY && !$rental->b_confirm_tx_hash)
+                                            <a href="{{route('rental.b_confirm', $rental)}}">You Should be Confirm</a>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_B_CONFIRM)
+                                            <span>Wait A Confirm</span>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_A_CONFIRM)
+                                            <span>Wait A Complete</span>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_A_COMPLETE && !$rental->b_complete_tx_hash)
+                                            <a href="{{route('rental.b_complete', $rental)}}">You Should Be Complete</a>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_COMPLETE)
+                                            <span>Complete</span>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_REJECT)
+                                            <span>Reject.Reason:{{$rental->reject_reason}}</span>
+                                        @endif
 
-                            <p>
-                                @if($rental->status == \App\Models\Rental::STATUS_A_APPLY && !$rental->b_confirm_tx_hash)
-                                    <a href="{{route('rental.b_confirm', $rental)}}">You Should be Confirm</a>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_B_CONFIRM)
-                                    <span>Wait A Confirm</span>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_A_CONFIRM)
-                                    <span>Wait A Complete</span>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_A_COMPLETE && !$rental->b_complete_tx_hash)
-                                    <a href="{{route('rental.b_complete', $rental)}}">You Should Be Complete</a>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_COMPLETE)
-                                    <span>Complete</span>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_REJECT)
-                                    <span>Reject.Reason:{{$rental->reject_reason}}</span>
-                                @endif
-                            </p>
+                                    @else
 
-                            @else
+                                        @if($rental->status == \App\Models\Rental::STATUS_A_APPLY)
+                                            <span>Wait B Confirm</span>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_B_CONFIRM && !$rental->a_confirm_tx_hash)
+                                            <a href="{{route('rental.a_confirm', $rental)}}">You Should be Confirm</a>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_A_CONFIRM && !$rental->a_complete_apply_tx_hash)
+                                            <a href="{{route('rental.a_complete', $rental)}}">Complete Apply</a>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_A_COMPLETE)
+                                            <span>WAIT B CONFIRM</span>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_COMPLETE)
+                                            <span>Complete</span>
+                                        @elseif($rental->status == \App\Models\Rental::STATUS_REJECT)
+                                            <span>Reject.Reason:{{$rental->reject_reason}}</span>
+                                        @endif
 
-                            <p>
-                                @if($rental->status == \App\Models\Rental::STATUS_A_APPLY)
-                                    <span>Wait B Confirm</span>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_B_CONFIRM && !$rental->a_confirm_tx_hash)
-                                    <a href="{{route('rental.a_confirm', $rental)}}">You Should be Confirm</a>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_A_CONFIRM && !$rental->a_complete_apply_tx_hash)
-                                    <a href="{{route('rental.a_complete', $rental)}}">Complete Apply</a>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_A_COMPLETE)
-                                    <span>WAIT B CONFIRM</span>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_COMPLETE)
-                                    <span>Complete</span>
-                                @elseif($rental->status == \App\Models\Rental::STATUS_REJECT)
-                                    <span>Reject.Reason:{{$rental->reject_reason}}</span>
-                                @endif
-                            </p>
-
-                        @endif
+                                    @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Charge: {{$rental->charge}} seele</td>
+                                <td>Deposit Money: {{$rental->deposit}} seele</td>
+                                <td>{{$rental->statusText()}}</td>
+                            </tr>
+                            @if($rental->a_apply_tx_hash)
+                                <tr><td colspan="3">A Apply Hash:{{$rental->a_apply_tx_hash}}</td></tr>
+                            @endif
+                            @if($rental->b_confirm_tx_hash)
+                                <tr>
+                                    <td colspan="3">
+                                        B Confirm Hash:{{$rental->b_confirm_tx_hash}}
+                                    </td>
+                                </tr>
+                            @endif
+                            @if($rental->a_confirm_tx_hash)
+                                <tr>
+                                    <td colspan="3">
+                                        A Confirm Hash:{{$rental->a_confirm_tx_hash}}
+                                    </td>
+                                </tr>
+                            @endif
+                            @if($rental->a_complete_apply_tx_hash)
+                                <tr>
+                                    <td colspan="3">
+                                        A Complete Hash:{{$rental->a_complete_apply_tx_hash}}
+                                    </td>
+                                </tr>
+                            @endif
+                            @if($rental->b_complete_tx_hash)
+                                <tr>
+                                    <td colspan="3">
+                                        B Complete Hash:{{$rental->b_complete_tx_hash}}
+                                    </td>
+                                </tr>
+                            @endif
+                        </table>
 
                     </div>
                 </div>
